5282bfe12b30120136471812f31f17fb1c3392bb
Mel Gibson <mgibson609@gmail.com>
2017-06-06 18:25:55 +0300

    Enemy made move. And spawn more enemy. MORE enemy. Game is hard git gud

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public int EnemyType;
+    public GameObject enemyPrefab;
+    public float speed;
+    private int dir = 0;
+
+
+	// Use this for initialization
+	void Start () {       
+        //Invoke("CreateEnemy", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveEnemy();
+	}
+
+    void MoveEnemy()
+    {
+        //gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right*speed*Time.deltaTime);
+
+        if (EnemyType==0) {
+            if (dir == 0)
+            {
+                gameObject.GetComponent<Transform>().Translate(Vector2.right * speed * Time.deltaTime);
+                if (gameObject.transform.position.x > 11)
+                {
+                    dir = 1;
+                }
+            }
+            if (dir == 1)
+            {
+                gameObject.GetComponent<Transform>().Translate(Vector2.left * speed * Time.deltaTime);
+                if (gameObject.transform.position.x < -11)
+                {
+                    dir = 0;
+                }
+            }
+        }
+
+    }
+    void CreateEnemy()
+    {
+        //Instantiate(enemyPrefab, new Vector2(-11f, Random.Range(-5f,5f)), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+
+    public GameObject enemyPrefab;
+    private float spawntimer = 5; // 5 seconds between enemies, counting down 1 seconds per 15 seconds
+	// Use this for initialization
+	void Start () {
+        Invoke("CreateEnemy", 1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void CreateEnemy()
+    {
+
+        Instantiate(enemyPrefab, new Vector2(-11f, Random.Range(-5f, 5f)), Quaternion.identity);
+        Invoke("CreateEnemy", spawntimer);
+        spawntimer = spawntimer - Time.deltaTime*15; //each 1 takes 15 seconds
+        Debug.Log(spawntimer);
+    }
+
+}
