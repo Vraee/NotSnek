62fd24397301a02697629c5a97d4833afd969f7e
Vrae <veera.tikkamaki@gmail.com>
2017-06-08 15:36:07 +0300

    Particle effect added to player in MoveTest, HP for each body part (10), started player's attack

--- Assets/Scripts/BodyPart.cs
+++ Assets/Scripts/BodyPart.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BodyPartCollision : MonoBehaviour {
+public class BodyPart : MonoBehaviour {
 
 	private CharacterController parentScript;
+	private int bodyPartHP;
 
 	void Start () {
 		parentScript = GameObject.Find ("Player").GetComponent<CharacterController> ();
+		bodyPartHP = parentScript.HPPerBodypart;
 	}
 
 	public void OnTriggerEnter2D(Collider2D collider) {
 		if (this.gameObject.tag == "Head" && collider.gameObject.tag == "PowerUp")
 			parentScript.AddBodyPart ();
 		else if (!(this.gameObject.tag == "Head") && !(this.gameObject.tag == "Tail") && collider.gameObject.tag == "Enemy") {
-			parentScript.RemoveBodyPart (this.gameObject);
+			int enemyDamage = 1;
+			bodyPartHP -= enemyDamage;
+			parentScript.SetHP(parentScript.GetHP() - enemyDamage);
+
+			if (bodyPartHP <= 0) {
+				parentScript.RemoveBodyPart (this.gameObject);
+			}
 		}
 	}
 }
