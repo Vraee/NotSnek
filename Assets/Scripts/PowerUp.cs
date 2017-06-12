dbdd2434df86e5f6585efd38763bc7a530cd4334
Vrae <veera.tikkamaki@gmail.com>
2017-06-12 15:23:38 +0300

    Andi fixed

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour {
 	public int collectibleValue = 2;
-    public GameObject target;
+	private GameObject target;
     public float fallSpeed;
     public float dragSpeed;
     private bool pulling;
+
+	void Start()
+	{
+		target = GameObject.Find ("Head");
+	}
 
     void Update()
     {
