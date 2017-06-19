5c6b434e1a76591dd41bec46aecb5c955989e568
Antities <akj.tuomisto@hotmail.com>
2017-06-19 15:06:54 +0300

    Just background scaling to camera size.

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -6,15 +6,27 @@ public class Scroll : MonoBehaviour {
 
     public float scrollSpeed;
     private Renderer rend;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         rend = GetComponent<Renderer>();
-	}
+        ScaleToCamera();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         Vector2 offset = new Vector2(0, Time.time * scrollSpeed);
 
         rend.material.mainTextureOffset = offset;
+    }
+    private void ScaleToCamera()
+    {
+        Camera cam = Camera.main;
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+        Debug.Log(height);
+        Debug.Log(width);
+        transform.localScale = new Vector3(width, height, 1);
+        transform.position = new Vector3(0, 0, 1);
     }
 }
