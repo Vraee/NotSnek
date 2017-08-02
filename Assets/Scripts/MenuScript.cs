2ac20ffb72639d726d85c792af3e6d81fb89522b
Antities <akj.tuomisto@hotmail.com>
2017-08-02 15:45:55 +0300

    Better pause with a confirmation. Add Menumanager to your scene and set the buttons with it for "everything" to work.

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,6 +4,35 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MenuScript : MonoBehaviour {
+    private GameObject verificationPanel;
+    private GameObject gameManager;
+    private GameObject pauseBackground;
+    private Camera cam;
+    private enum PauseStates
+    {
+        pause,
+        verification,
+        none
+    }
+    private PauseStates current;
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+        pauseBackground = GameObject.Find("PauseMenu");
+        pauseBackground.SetActive(false);
+        verificationPanel = GameObject.Find("VerificationPanel");
+        verificationPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
 
     public void ChangeScene(int SceneIndex)
     {
@@ -13,5 +42,40 @@ public class MenuScript : MonoBehaviour {
     public void QuitApplication()
     {
         Application.Quit();
+    }
+
+    public void EnableVerification()
+    {
+        verificationPanel.SetActive(true);
+        current = PauseStates.verification;
+    }
+
+    public void DisableVerificationPanel()
+    {
+        verificationPanel.SetActive(false);
+        current = PauseStates.pause;
+    }
+
+    public void PauseGame()
+    {
+        if (current == PauseStates.none)
+        {
+            Time.timeScale = 0;
+            pauseBackground.SetActive(true);
+            current = PauseStates.pause;
+            cam.GetComponent<CameraShake>().SetShake(false);
+        }
+        else if (current == PauseStates.pause)
+        {
+            pauseBackground.SetActive(false);
+            Time.timeScale = 1;
+            current = PauseStates.none;
+            cam.GetComponent<CameraShake>().SetShake(true);
+        }
+        else if (current == PauseStates.verification)
+        {
+            current = PauseStates.pause;
+            verificationPanel.SetActive(false);
+        }
     }
 }
